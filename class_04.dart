7110e40c63d1172ffbb53a908db6bf0416ddb993
muhammadfasih001 <muhammadfasih006@gmail.com>
2023-06-23 17:42:13 +0500

    changes

--- class_04.dart
+++ class_04.dart
@@ -11,195 +11,257 @@ void main() {
   //   print("Num2 is greater than Num1");
   // }
 
-  List audiStudentNames = [
-    "fasih",
-    "hamza",
-    "bilal",
-    "jahangir",
-    0,
-    1,
-    2,
-    3,
-  ];
+  // List audiStudentNames = [
+  //   "fasih",
+  //   "hamza",
+  //   "bilal",
+  //   "jahangir",
+  //   0,
+  //   1,
+  //   2,
+  //   3,
+  // ];
 
-  var result = audiStudentNames.first;
-  print(result);
+  // var result = audiStudentNames.first;
+  // print(result);
 
-  var result2 = audiStudentNames.last;
-  print(result2);
+  // var result2 = audiStudentNames.last;
+  // print(result2);
 
-  print(audiStudentNames[1]);
+  // print(audiStudentNames[1]);
 
-  var result4 = audiStudentNames.elementAt(3);
-  print(result4);
+  // var result4 = audiStudentNames.elementAt(3);
+  // print(result4);
 
-  //Updating List.
+  // //Updating List.
 
-  List multipleValueNames = [1, 2, 3, 4, 5, "hamza", "fasih", "saif", "rohan"];
+  // List multipleValueNames = [1, 2, 3, 4, 5, "hamza", "fasih", "saif", "rohan"];
 
-  multipleValueNames[5] = 5;
-  multipleValueNames[5] = "shakir";
-  multipleValueNames[5] = 5;
-  print(multipleValueNames);
+  // multipleValueNames[5] = 5;
+  // multipleValueNames[5] = "shakir";
+  // multipleValueNames[5] = 5;
+  // print(multipleValueNames);
 
-  multipleValueNames
-      .replaceRange(2, 8, ["muhammad", "jahanzgir", 5, 8, 7, 10, 5]);
+  // multipleValueNames
+  //     .replaceRange(2, 8, ["muhammad", "jahanzgir", 5, 8, 7, 10, 5]);
 
-  print(multipleValueNames);
+  // print(multipleValueNames);
 
-  multipleValueNames.replaceRange(4, 6, ["new 2", "3 and 4"]);
-  print(multipleValueNames);
+  // multipleValueNames.replaceRange(4, 6, ["new 2", "3 and 4"]);
+  // print(multipleValueNames);
 
-  // if we dont know how much value in array so this method apply.
+  // // if we dont know how much value in array so this method apply.
 
-  multipleValueNames.replaceRange(multipleValueNames.length - 1,
-      multipleValueNames.length, ["muhammad rohan"]);
+  // multipleValueNames.replaceRange(multipleValueNames.length - 1,
+  //     multipleValueNames.length, ["muhammad rohan"]);
 
-  print(multipleValueNames);
+  // print(multipleValueNames);
 
-  // Sort Method.
+  // // Sort Method.
 
-  List<String> digitNumbers = [
-    "fasih",
-    "bilal",
-    "shakir",
-    "naved",
-    "abdullah qadiani"
-  ];
+  // List<String> digitNumbers = [
+  //   "fasih",
+  //   "bilal",
+  //   "shakir",
+  //   "naved",
+  //   "abdullah qadiani"
+  // ];
 
-  digitNumbers.sort((a, z) => a.length.compareTo(z.length));
-  print(digitNumbers);
+  // digitNumbers.sort((a, z) => a.length.compareTo(z.length));
+  // print(digitNumbers);
 
-  digitNumbers.sort();
-  print(digitNumbers);
+  // digitNumbers.sort();
+  // print(digitNumbers);
 
-  digitNumbers.sort((a, b) => b.compareTo(a));
-  print(digitNumbers);
+  // digitNumbers.sort((a, b) => b.compareTo(a));
+  // print(digitNumbers);
 
-  // Descending Order Sort Method.
+  // // Descending Order Sort Method.
 
-  List num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // List num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  num.sort((a, b) => b.compareTo(a));
-  print(num);
+  // num.sort((a, b) => b.compareTo(a));
+  // print(num);
 
-  //isEmpty & isNotEmpty Method.
+  // //isEmpty & isNotEmpty Method.
 
-  List checkList = [];
+  // List checkList = [];
 
-  if (checkList.isEmpty) {
-    print("$checkList List is an empty");
-  } else {
-    print("$checkList is not empty");
-  }
+  // if (checkList.isEmpty) {
+  //   print("$checkList List is an empty");
+  // } else {
+  //   print("$checkList is not empty");
+  // }
 
-  checkList.add(1);
-  checkList.add(2);
-  checkList.add(3);
-  checkList.add(4);
-  checkList.add(5);
-  checkList.add(6);
+  // checkList.add(1);
+  // checkList.add(2);
+  // checkList.add(3);
+  // checkList.add(4);
+  // checkList.add(5);
+  // checkList.add(6);
 
-  if (checkList.isEmpty) {
-    print("$checkList yes list is empty");
-  } else {
-    print("$checkList <= check List is not empty");
-  }
+  // if (checkList.isEmpty) {
+  //   print("$checkList yes list is empty");
+  // } else {
+  //   print("$checkList <= check List is not empty");
+  // }
 
-  checkList.clear();
+  // checkList.clear();
 
-  if (checkList.isEmpty) {
-    print("$checkList yes List is empty");
-  } else {
-    print("$checkList List i not empty");
-  }
+  // if (checkList.isEmpty) {
+  //   print("$checkList yes List is empty");
+  // } else {
+  //   print("$checkList List i not empty");
+  // }
 
-  //Reversed Method.
+  // //Reversed Method.
 
-  List alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"];
+  // List alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"];
 
-  alphabet = List.of(alphabet.reversed);
-  print(alphabet);
+  // alphabet = List.of(alphabet.reversed);
+  // print(alphabet);
 
-  var reversed = List.of(alphabet.reversed);
+  // var reversed = List.of(alphabet.reversed);
 
-  print(reversed);
+  // print(reversed);
 
-  //Add Method.
+  // //Add Method.
 
-  List addValue = [1, 2, 3, 4, 5];
+  // List addValue = [1, 2, 3, 4, 5];
 
-  addValue.add(6);
-  print(addValue);
+  // addValue.add(6);
+  // print(addValue);
 
-  addValue.addAll([5, 2, 4, 8, 2, 14, 5]);
-  print(addValue);
+  // addValue.addAll([5, 2, 4, 8, 2, 14, 5]);
+  // print(addValue);
 
-  //Insert Method.
+  // //Insert Method.
 
-  List insertValue = [1, 2, 3, 4, 5];
+  // List insertValue = [1, 2, 3, 4, 5];
 
-  insertValue.insert(0, 0);
+  // insertValue.insert(0, 0);
 
-  print(insertValue);
+  // print(insertValue);
 
-  insertValue.insertAll(0, [12, 13, 14]);
+  // insertValue.insertAll(0, [12, 13, 14]);
 
-  print(insertValue);
+  // print(insertValue);
 
-  //Remove Method.
+  // //Remove Method.
 
-  List<int> values = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // List<int> values = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  values.remove(5);
+  // values.remove(5);
 
-  print(values);
+  // print(values);
 
-  values.replaceRange(4, 4, [5]);
-  print(values);
+  // values.replaceRange(4, 4, [5]);
+  // print(values);
 
-  values.removeAt(0);
-  print(values);
+  // values.removeAt(0);
+  // print(values);
 
-  values.insert(0, 1);
-  print(values);
+  // values.insert(0, 1);
+  // print(values);
 
-  // if we dont know how much value in array so this method apply.
+  // // if we dont know how much value in array so this method apply.
 
-  values.removeAt((values.length - 1));
-  print(values);
+  // values.removeAt((values.length - 1));
+  // print(values);
 
-  values.removeLast();
-  print(values);
+  // values.removeLast();
+  // print(values);
 
-  values.clear();
-  print(values);
+  // values.clear();
+  // print(values);
 
-  // List removeWhere & removeretain.
+  // // List removeWhere & removeretain.
 
-  List digits = [5, 25, 4, 5, 21, 85, 3, 6];
+  // List digits = [5, 25, 4, 5, 21, 85, 3, 6];
 
-  // while condition is true then remove the element otherwise condition is false then print.
-  // greatest number print.
+  // // while condition is true then remove the element otherwise condition is false then print.
+  // // greatest number print.
 
-  digits.removeWhere((numbers) => numbers < 7);
-  print(digits);
+  // digits.removeWhere((numbers) => numbers < 7);
+  // print(digits);
 
-  // while condition is false then remove the element otherwise condition is true then print.
-  // Lowest number print.
+  // // while condition is false then remove the element otherwise condition is true then print.
+  // // Lowest number print.
 
-  List digits1 = [5, 25, 4, 5, 21, 85, 3, 6];
+  // List digits1 = [5, 25, 4, 5, 21, 85, 3, 6];
 
-  digits1.retainWhere((numbers) => numbers < 7);
-  print(digits1);
+  // digits1.retainWhere((numbers) => numbers < 7);
+  // print(digits1);
 
-  //List forEach method.
+  // // List forEach method.
 
-  List<String> allName = ["bilal", "fasih", "kaif"];
+  // List<String> allName = ["bilal", "fasih", "kaif"];
 
-  allName.forEach((allName) => print(allName));
+  // //one way use forEach method.
+  // allName.forEach((allName) {
+  //   print(allName);
+  // });
 
-  allName.forEach((allName) {
-    print(allName);
-  });
+  // allName.add("saif");
+  // print(allName);
+
+  // allName.forEach((name) => print(name));
+
+  // //second way use forEach method.
+  // allName.forEach((name) => print(name));
+
+  // List<int> allNumber = [
+  //   2,
+  //   5,
+  //   11,
+  //   77,
+  //   56,
+  // ];
+
+  // int sumAllNumber = 0;
+
+  // allNumber.forEach((num) => sumAllNumber += num);
+  // print(sumAllNumber);
+
+  //contain Method boolean print like true and false.
+
+  // List friendName = ["muhammad", "saif", "haider", "ali", "umer", 1, 52];
+  // bool check;
+
+  // check = friendName.contains(5);
+  // print(check);
+  // check = friendName.contains(52);
+  // print(check);
+
+  // where and startwith method.
+
+  // List<String> friendNames = ["muhammad", "saif", "haider", "ali", "umer"];
+
+  // var filter = friendNames.where((name) => name.contains("a"));
+  // print(filter);
+
+  // var filters = friendNames.where((name) => name.startsWith("a"));
+  // print(filters);
+
+  // index where method return index value like integer.
+
+  // List<String> cricketTeam = [
+  //   "pakistan",
+  //   "england",
+  //   "new zealand",
+  //   "bangladesh"
+  // ];
+
+  // int result = cricketTeam.indexWhere((team) => team.startsWith("b"));
+  // print(result);
+
+  //firstWhere lastWhere method.
+
+  List number = [5, 4, 8, 25, 9, 6, 4, 1];
+
+  // var check = number.firstWhere((element) => element < 25);
+  // print(check);
+
+  var check2 = number.lastWhere((num) => num > 4);
+  print(check2);
 }
--- class_5.dart
+++ class_5.dart
@@ -1,3 +1,39 @@
 void main() {
-  print("Hello World.");
+  // List studentRecord = [
+  //   {
+  //     "name": ["fasih", "mushtaba"],
+  //     "fatherName": "Muhammad Sadiq",
+  //     "Roll number": 109,
+  //   },
+  //   {
+  //     "name": "ali",
+  //     "fatherName": "shan",
+  //     "Roll Number": 5,
+  //   }
+  // ];
+
+  // print(studentRecord[0]);
+  // print(studentRecord[0]["name"]);
+  // print(studentRecord[0]["fatherName"]);
+
+  Map audiRecord = {
+    "SecA": {
+      "name": "MuhammadFasih",
+      "FathersName": "Muhammad sadiq",
+      "Roll number": 12
+    },
+    "SecB": {
+      "name": "MuhammadAhmed",
+      "FathersName": "Muhammad shahroz",
+      "Roll number": 14
+    },
+    "SecC": {
+      "name": "Muhammadjahanzaib",
+      "FathersName": "Muhammad shakeel",
+      "Roll number": 45
+    }
+  };
+
+  print(audiRecord["SecA"]);
+  print(audiRecord["SecA"]["name"]);
 }
